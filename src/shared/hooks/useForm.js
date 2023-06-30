6bae5ccf1c40d63085f83208d8070b7d7584224f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-30 21:08:19 +0300

    add reset function

--- src/shared/hooks/useForm.js
+++ src/shared/hooks/useForm.js
@@ -1,24 +1,29 @@
-import {useState, useCallback} from "react";
+import { useState, useCallback } from 'react';
 
-const useForm = ({initialState, onSubmit})=> {
-    const [state, setState] = useState({...initialState});
+const useForm = ({ initialState, onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
 
-    const handleChange = useCallback(({ target }) => {
-        setState(prevState => {
-            const { name, value, checked, type } = target;
-            const newValue = type === "checkbox" ? checked : value;
+  const handleChange = useCallback(
+    ({ target }) => {
+      setState((prevState) => {
+        const { name, value, checked, type } = target;
+        const newValue = type === 'checkbox' ? checked : value;
 
-            return {...prevState, [name]: newValue}
-        })
-    }, [setState]);
+        return { ...prevState, [name]: newValue };
+      });
+    },
+    [setState]
+  );
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit({...state});
-        setState({...initialState});
-    };
+  const reset = useCallback(() => setState({ ...initialState }), [initialState]);
 
-    return {state, setState, handleChange, handleSubmit};
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ ...state });
+    reset();
+  };
+
+  return { state, setState, handleChange, handleSubmit, reset };
+};
 
 export default useForm;

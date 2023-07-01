360ddd5b6eff51e9fda3fe946e6945b0b47bb401
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-01 22:37:13 +0300

    update

--- src/modules/MyMovies/MyMoviesList/MyMoviesList.jsx
+++ src/modules/MyMovies/MyMoviesList/MyMoviesList.jsx
@@ -1,24 +1,21 @@
 import styles from './my-books-list.module.scss';
+
 const { REACT_APP_FILE_URL } = process.env;
-console.log(REACT_APP_FILE_URL);
+// console.log(REACT_APP_FILE_URL);
 
 const MyMoviesList = ({ items, onDeleteMovie }) => {
   const elements = items.map(({ _id, title, director, poster }) => (
-    <ul className={styles.listItem} key={_id}>
-      <li className={styles.listItem} key={_id}>
-        {poster && (
-          <img
-            className={styles.listItemPoster}
-            src={`${REACT_APP_FILE_URL}/${poster}`}
-            alt='poster'
-          />
-        )}
-        Title: {title}. Director: {director}.{' '}
-        <button onClick={() => onDeleteMovie(_id)}>delete</button>
-      </li>
+    <li className={styles.listItem} key={_id}>
+      {poster && (
+        <img
+          className={styles.listItemPoster}
+          src={`${REACT_APP_FILE_URL}/${poster}`}
+          alt='poster'
+        />
+      )}
       Title: {title}. Director: {director}.{' '}
       <button onClick={() => onDeleteMovie(_id)}>delete</button>
-    </ul>
+    </li>
   ));
 
   return <ol className={styles.list}>{elements}</ol>;
